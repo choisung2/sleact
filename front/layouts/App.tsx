856da2d82fa57cc6eeaf3c016a646da7635dcf9e
choisung2 <c0817d@gmail.com>
2022-05-09 02:12:55 +0900

    ✨ workspace, channel 페이지 추가

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import loadable from '@loadable/component';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LogIn />} />
       <Route path="/login" element={<LogIn />} />
-      <Route path="/login" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/workspace/channel" element={<Channel />} />
+      <Route path="/redirect" element={<Navigate to="/login" />} />
     </Routes>
   );
 };
