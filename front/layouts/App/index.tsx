35080c5b3c95396ace5adc9581eaec1fd6d5181e
choisung2 <c0817d@gmail.com>
2022-05-20 23:38:19 +0900

    🚧 dm 및 모달 세팅

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -5,6 +5,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/login" element={<LogIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/workspace/channel" element={<Channel />} />
+      <Route path="/workspace/dm" element={<DirectMessage />} />
       <Route path="/redirect" element={<Navigate to="/login" />} />
     </Routes>
   );
